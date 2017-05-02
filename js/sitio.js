d44e79cf0e10c416749b9a2a11005210578f3166
jtirado@seek.pe <jordy_unt@live.com>
2017-05-02 17:33:21 -0500

    add cabezera animada

--- js/sitio.js
+++ js/sitio.js
@@ -23,3 +23,18 @@ $(function () {
     });
 
 });
+
+/*---------------------------------
+   CABECERA ANIMADA
+ ----------------------------------*/
+$(window).scroll(function () {
+
+    var nav = $('.encabezado');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+        nav.addClass("fondo-menu");
+    } else {
+        nav.removeClass("fondo-menu");
+    }
+});
